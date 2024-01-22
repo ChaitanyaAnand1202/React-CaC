645d79b6277a5972a8b8ddc14afe9b82e11bc50b
CHAITANYA ANAND <anandchaitanya12@gmail.com>
2024-01-23 00:07:07 +0530

    learnt react.createElement

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function myApp(){
+  return (
+    <h1>Let's study react</h1>
+  )
+}
+
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "blank"
+//   },
+//   content: "Click here to got to google"
+// }
+
+const reactElement = (
+  <a href='https://www.google.com' target='blank'>Search with google</a>
+)
+
+const newElement = React.createElement(
+  "h3",
+  {
+    className:"heading",
+    id:"react-element"
+  },
+  "Hello there, learn react?"
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  newElement
+
 )
